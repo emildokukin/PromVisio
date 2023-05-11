dc746ebc56cae423ae44a3ce42f93794c5083267
Bombazok <sergeipiskarev@yandex.ru>
2023-05-11 09:38:42 +0300

    Fix all issues

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -2,15 +2,12 @@ import styles from './HomePage.module.scss'
 import Page from '../../common/page/Page'
 import {Helmet} from 'react-helmet-async'
 
-
 const HomePage = () => {
   return (
     <Page className={styles.page}>
       <Helmet>
         <title>Главная</title>
       </Helmet>
-
-      
     </Page>
   )
 }
