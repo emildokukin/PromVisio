561cb2628339006d9dde5c02309b88d709302637
Bombazok <sergeipiskarev@yandex.ru>
2023-05-11 22:36:00 +0300

    Add Footer

--- src/components/common/link-component/LinkComponent.tsx
+++ src/components/common/link-component/LinkComponent.tsx
@@ -1,21 +1,27 @@
+import clsx from 'clsx'
 import {ReactNode} from 'react'
 import {Link} from 'react-router-dom'
 
 interface LinkComponentProps {
-  link: string
-  linkExternal: boolean
+  link?: string
+  linkExternal?: boolean
   children: ReactNode
+  className?: string
 }
 
-const LinkComponent = ({link, linkExternal, children}: LinkComponentProps) => {
+const LinkComponent = ({link, linkExternal, children, className}: LinkComponentProps) => {
   if (link && linkExternal) {
     return (
-      <a href={link} target='_blank' rel='noreferrer'>
+      <a href={link} target='_blank' rel='noreferrer' className={clsx(className)}>
         {children}
       </a>
     )
   } else if (link) {
-    return <Link to={link}>{children}</Link>
+    return (
+      <Link to={link} className={clsx(className)}>
+        {children}
+      </Link>
+    )
   }
 
   return <>{children}</>
--- src/components/common/page/Footer.tsx
+++ src/components/common/page/Footer.tsx
@@ -1,7 +1,43 @@
+import HireButton from '../hire-button/HireButton'
+import LinkComponent from '../link-component/LinkComponent'
 import styles from './Footer.module.scss'
 
 const Footer = () => {
-  return <footer className={styles.footer} id='footer'></footer>
+  return (
+    <footer className={styles.footer} id='footer'>
+      <div className={styles.info}>
+        <div className={styles.socials}>
+          <LinkComponent link='#' linkExternal className={styles.social}>
+            <img src='/icons/dzen.svg' alt='dzen' />
+          </LinkComponent>
+
+          <LinkComponent link='#' linkExternal className={styles.social}>
+            <img src='/icons/youtube.svg' alt='youtube' />
+          </LinkComponent>
+
+          <LinkComponent link='emailto:info@promvisio.ru' className={styles.email}>
+            <h2>info@promvisio.ru</h2>
+          </LinkComponent>
+        </div>
+
+        <hr />
+
+        <p className={styles.copyright}>© 2023 «ПРОМВИЗИО»</p>
+      </div>
+
+      <div className={styles.action}>
+        <LinkComponent link='tel:+79039689048' className={styles.phone}>
+          <div className={styles.social}>
+            <img src='/icons/phone-footer.svg' alt='phone' />
+          </div>
+
+          <h2>+7 (903) 968 9048</h2>
+        </LinkComponent>
+
+        <HireButton />
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
