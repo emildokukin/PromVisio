1ce2d0913cfe1f0a9e462db165ae8f692499d759
Bombazok <sergeipiskarev@yandex.ru>
2023-05-12 16:21:01 +0300

    Handle body overflow on Modal state update

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import {ReactNode} from 'react'
+import {ReactNode, useEffect} from 'react'
 import styles from './Modal.module.scss'
 import {ReactComponent as Cross} from '../../../icons/cross.svg'
 
@@ -12,34 +12,42 @@ interface ModalProps {
   onSubmit?: () => void
 }
 
-const Modal = ({active, toggle, children, title, image, onSubmit}: ModalProps) => (
-  <div className={clsx(styles.modal, {[styles.modalActive]: active})} onClick={toggle}>
-    <div className={clsx(styles.content, {[styles.contentActive]: active})} onClick={(e) => e.stopPropagation()}>
-      <div className={styles.logo}>
-        <img src={image} alt='logo' />
-      </div>
+const Modal = ({active, toggle, children, title, image, onSubmit}: ModalProps) => {
+  useEffect(() => {
+    if (document) {
+      document.body.style.overflow = active ? 'hidden' : 'auto'
+    }
+  }, [active])
 
-      <form className={styles.form} onSubmit={onSubmit}>
-        <h2>{title}</h2>
-
-        <div className={styles.inputs}>{children}</div>
-
-        <div className={styles.bottom}>
-          <button type='submit' className={styles.button}>
-            ОТПРАВИТЬ
-          </button>
-
-          <p className={styles.important}>
-            Очень важные поля для заполнения <img src='/icons/asterisk.svg' alt='asterisk' />
-          </p>
+  return (
+    <div className={clsx(styles.modal, {[styles.modalActive]: active})} onClick={toggle}>
+      <div className={clsx(styles.content, {[styles.contentActive]: active})} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.logo}>
+          <img src={image} alt='logo' />
         </div>
-      </form>
 
-      <div className={styles.cross} onClick={toggle}>
-        <Cross />
+        <form className={styles.form} onSubmit={onSubmit}>
+          <h2>{title}</h2>
+
+          <div className={styles.inputs}>{children}</div>
+
+          <div className={styles.bottom}>
+            <button type='submit' className={styles.button}>
+              ОТПРАВИТЬ
+            </button>
+
+            <p className={styles.important}>
+              Очень важные поля для заполнения <img src='/icons/asterisk.svg' alt='asterisk' />
+            </p>
+          </div>
+        </form>
+
+        <div className={styles.cross} onClick={toggle}>
+          <Cross />
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Modal
