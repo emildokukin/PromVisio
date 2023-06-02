236d51f88a1c7352bb6db67ec643c0a9051edbef
EmilDok <kyrbicanal2@gmail.com>
2023-06-02 22:42:05 +0300

    Rename gazprom link folder

--- src/components/common/gazprom-link/GazpromLink.tsx
+++ src/components/common/gazprom-link/GazpromLink.tsx
@@ -1,14 +1,16 @@
+import clsx from 'clsx'
 import globeSvg from '../../../icons/globe.svg'
 import LinkComponent from '../link-component/LinkComponent'
 import styles from './GazpromLink.module.scss'
 
 interface GazpromLink {
   linkToGazprom: string
+  className?: string
 }
 
-const GazpromLink = ({linkToGazprom}: GazpromLink) => {
+const GazpromLink = ({linkToGazprom, className}: GazpromLink) => {
   return (
-    <object>
+    <object className={clsx(className)}>
       <img className={styles.globeSvg} src={globeSvg} alt='globe' />
       <LinkComponent className={styles.linkToGazprom} link={linkToGazprom} linkExternal={true}>
         Новость на сайте Газпрома
