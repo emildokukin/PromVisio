2e9493f940b670076683d1975a8f71ddca4f33ad
EmilDok <kyrbicanal2@gmail.com>
2023-06-04 02:05:17 +0300

    Change color changable globe news inner link

--- src/components/common/news-inner-link/NewsInnerLink.tsx
+++ src/components/common/news-inner-link/NewsInnerLink.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx'
-import globeSVG from '../../../icons/globe.svg'
+import {ReactComponent as GlobeSVG} from '../../../icons/globe.svg'
 import LinkComponent from '../link-component/LinkComponent'
 import styles from './NewsInnerLink.module.scss'
 
@@ -10,7 +10,7 @@ interface NewsInnerLink {
 
 const NewsInnerLink = ({link, className}: NewsInnerLink) => (
   <object className={clsx(className)}>
-    <img className={styles.globe} src={globeSVG} alt='globe' />
+    <GlobeSVG className={styles.globe} />
     <LinkComponent className={styles.link} link={link} linkExternal={true}>
       Новость на сайте Газпрома
     </LinkComponent>
