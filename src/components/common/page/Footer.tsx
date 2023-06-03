fc613c4e37a3554efe260a2741b57f40255be788
Bombazok <sergeipiskarev@yandex.ru>
2023-06-03 17:01:43 +0300

    Add Footer hover effects

--- src/components/common/page/Footer.tsx
+++ src/components/common/page/Footer.tsx
@@ -1,15 +1,17 @@
 import LinkComponent from '../link-component/LinkComponent'
 import styles from './Footer.module.scss'
 import {ReactComponent as PhoneSVG} from '../../../icons/phone-small.svg'
-import {ReactComponent as Envelope} from '../../../icons/envelope.svg'
+import {ReactComponent as EnvelopeSVG} from '../../../icons/envelope.svg'
+import {ReactComponent as DzenSVG} from '../../../icons/dzen.svg'
+import {ReactComponent as YoutubeSVG} from '../../../icons/youtube.svg'
 import useMedia from '../../utils/useMedia'
 import clsx from 'clsx'
 
 export const Email = ({isMobile}: {isMobile?: boolean}) => (
-  <LinkComponent link='emailto:info@promvisio.ru' className={styles.email}>
+  <LinkComponent link='emailto:info@promvisio.ru' className={styles.emailWrapper}>
     {isMobile && (
-      <div className={styles.social}>
-        <Envelope />
+      <div className={styles.email}>
+        <EnvelopeSVG />
       </div>
     )}
 
@@ -17,9 +19,9 @@ export const Email = ({isMobile}: {isMobile?: boolean}) => (
   </LinkComponent>
 )
 
-export const Phone = () => (
-  <LinkComponent link='tel:+79039689048' className={styles.phone}>
-    <div className={styles.social}>
+export const Phone = ({isDesktop}: {isDesktop?: boolean}) => (
+  <LinkComponent link='tel:+79039689048' className={styles.phoneWrapper}>
+    <div className={clsx(styles.phone, {[styles.social]: isDesktop})}>
       <PhoneSVG />
     </div>
 
@@ -43,11 +45,11 @@ const Footer = () => {
       <div className={styles.info}>
         <div className={styles.socials}>
           <LinkComponent link='#' linkExternal className={styles.social}>
-            <img src='/icons/dzen.svg' alt='dzen' /> {isMobile && <span>Дзен</span>}
+            <DzenSVG /> {isMobile && <span>Дзен</span>}
           </LinkComponent>
 
           <LinkComponent link='#' linkExternal className={styles.social}>
-            <img src='/icons/youtube.svg' alt='youtube' /> {isMobile && <span>YouTube</span>}
+            <YoutubeSVG /> {isMobile && <span>YouTube</span>}
           </LinkComponent>
 
           {isDesktop && <Email />}
@@ -59,7 +61,7 @@ const Footer = () => {
       </div>
 
       <div className={styles.action}>
-        <Phone />
+        <Phone isDesktop={isDesktop} />
 
         {isMobile && <Email isMobile />}
       </div>
