3a994ab76a9da7e676d09b2b65b79a9ddab0518c
Bombazok <sergeipiskarev@yandex.ru>
2023-06-05 00:33:50 +0300

    Move inputs to the FormModal component

--- src/components/common/hire-button/HireButton.tsx
+++ src/components/common/hire-button/HireButton.tsx
@@ -1,19 +1,12 @@
-import {ChangeEvent, useCallback, useState} from 'react'
+import {useCallback, useState} from 'react'
 import styles from './HireButton.module.scss'
 import FormModal from '../modal/FormModal'
 import clsx from 'clsx'
-import {Input} from '../input/Input'
-import {Textarea} from '../input/Textarea'
-import {getPhoneNumber} from '../../utils/getPhoneNumber'
 
 const HireButton = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
 
   const toggleModalVisibility = useCallback(() => setIsModalVisible((prev) => !prev), [])
-
-  const handlePhoneChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    getPhoneNumber(e)
-  }, [])
 
   return (
     <>
@@ -21,19 +14,7 @@ const HireButton = () => {
         <span>Нанять нас</span> <img src='/icons/phone.svg' alt='phone' />
       </button>
 
-      <FormModal active={isModalVisible} toggle={toggleModalVisibility} title='Нанять нас' image='/icons/logo.svg'>
-        <Input label='Тема' name='theme' type='text' required />
-        <Textarea label='Текст сообщения' name='message' />
-        <Input
-          label='Номер телефона'
-          name='phone_number'
-          placeholder='+7'
-          type='tel'
-          required
-          onChange={handlePhoneChange}
-        />
-        <Input label='Email' name='email' type='email' required />
-      </FormModal>
+      <FormModal active={isModalVisible} toggle={toggleModalVisibility} title='Нанять нас' image='/icons/logo.svg' />
     </>
   )
 }
--- src/components/common/modal/FormModal.tsx
+++ src/components/common/modal/FormModal.tsx
@@ -1,37 +1,60 @@
+import {ChangeEvent, useCallback} from 'react'
+import {Input} from '../input/Input'
+import {Textarea} from '../input/Textarea'
 import {CloseButton} from './CloseButton'
 import styles from './FormModal.module.scss'
 import Modal, {ModalBasedProps} from './Modal'
+import {getPhoneNumber} from '../../utils/getPhoneNumber'
+import clsx from 'clsx'
 
 interface FormModalProps extends ModalBasedProps {
   title: string
   image: string
   onSubmit?: () => void
+  className?: string
 }
 
-const FormModal = ({active, toggle, children, title, image, onSubmit}: FormModalProps) => (
-  <Modal active={active} toggle={toggle} contentClassName={styles.content}>
-    <div className={styles.logo}>
-      <img src={image} alt='logo' />
-    </div>
+const FormModal = ({active, toggle, title, image, onSubmit, className}: FormModalProps) => {
+  const handlePhoneChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    getPhoneNumber(e)
+  }, [])
 
-    <form className={styles.form} onSubmit={onSubmit}>
-      <h2>{title}</h2>
-
-      <div className={styles.inputs}>{children}</div>
-
-      <div className={styles.bottom}>
-        <button type='submit' className={styles.button}>
-          ОТПРАВИТЬ
-        </button>
-
-        <p className={styles.important}>
-          Очень важные поля для заполнения <img src='/icons/asterisk.svg' alt='asterisk' />
-        </p>
+  return (
+    <Modal active={active} toggle={toggle} contentClassName={clsx(styles.content, className)}>
+      <div className={styles.logo}>
+        <img src={image} alt='logo' />
       </div>
-    </form>
 
-    <CloseButton onClick={toggle} />
-  </Modal>
-)
+      <form className={styles.form} onSubmit={onSubmit}>
+        <h2>{title}</h2>
 
+        <div className={styles.inputs}>
+          <Input label='Тема' name='theme' type='text' required />
+          <Textarea label='Текст сообщения' name='message' />
+          <Input
+            label='Номер телефона'
+            name='phone_number'
+            placeholder='+7'
+            type='tel'
+            required
+            onChange={handlePhoneChange}
+          />
+          <Input label='Email' name='email' type='email' required />
+        </div>
+
+        <div className={styles.bottom}>
+          <button type='submit' className={styles.button}>
+            ОТПРАВИТЬ
+          </button>
+
+          <p className={styles.important}>
+            Очень важные поля для заполнения <img src='/icons/asterisk.svg' alt='asterisk' />
+          </p>
+        </div>
+      </form>
+
+      <CloseButton onClick={toggle} />
+    </Modal>
+  )
+}
 export default FormModal
