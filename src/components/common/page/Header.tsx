f6d20920ab294e4ed2e5cf737def174570ca6331
Bombazok <sergeipiskarev@yandex.ru>
2023-06-05 01:36:44 +0300

    Open Mobile menu on location update

--- src/components/common/page/Header.tsx
+++ src/components/common/page/Header.tsx
@@ -1,33 +1,29 @@
 import LinkComponent from '../link-component/LinkComponent'
 import styles from './Header.module.scss'
-import {useCallback, useState} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import useMedia from '../../utils/useMedia'
 import clsx from 'clsx'
 import {Copyright as FooterCopyright, Email as FooterEmail, Phone as FooterPhone} from './Footer'
 import {useLocation} from 'react-router-dom'
 
-const Links = () => {
-  const location = useLocation().pathname
-
-  return (
-    <nav className={styles.links}>
-      <ul>
-        <li className={clsx({[styles.current]: location === '/gallery'})}>
-          <LinkComponent link='/gallery'>Галерея</LinkComponent>
-        </li>
-        <li className={clsx({[styles.current]: location === '/news'})}>
-          <LinkComponent link='/news'>Вестник</LinkComponent>
-        </li>
-        <li className={clsx({[styles.current]: location === '/project'})}>
-          <LinkComponent link='/project'>История одного проекта</LinkComponent>
-        </li>
-        <li className={clsx(styles.border, {[styles.current]: location === '/potential'})}>
-          <LinkComponent link='/potential'>Потенциал ПРО</LinkComponent>
-        </li>
-      </ul>
-    </nav>
-  )
-}
+const Links = ({location}: {location?: string}) => (
+  <nav className={styles.links}>
+    <ul>
+      <li className={clsx({[styles.current]: location === '/gallery'})}>
+        <LinkComponent link='/gallery'>Галерея</LinkComponent>
+      </li>
+      <li className={clsx({[styles.current]: location === '/news'})}>
+        <LinkComponent link='/news'>Вестник</LinkComponent>
+      </li>
+      <li className={clsx({[styles.current]: location === '/project'})}>
+        <LinkComponent link='/project'>История одного проекта</LinkComponent>
+      </li>
+      <li className={clsx(styles.border, {[styles.current]: location === '/potential'})}>
+        <LinkComponent link='/potential'>Потенциал ПРО</LinkComponent>
+      </li>
+    </ul>
+  </nav>
+)
 
 const Menu = ({isVisible}: {isVisible?: boolean}) => (
   <div className={clsx(styles.menu, {[styles.visible]: isVisible})}>
@@ -48,6 +44,7 @@ const Hamburger = ({isTriggered, onClick}: {isTriggered: boolean; onClick: () =>
 const Header = () => {
   const [isOpened, setIsOpened] = useState(false)
   const {isDesktop, isMobile} = useMedia()
+  const location = useLocation().pathname
 
   const triggerMenuState = useCallback(() => {
     if (document) {
@@ -57,6 +54,8 @@ const Header = () => {
     setIsOpened((prev) => !prev)
   }, [isOpened])
 
+  useEffect(() => setIsOpened(false), [location])
+
   return (
     <>
       <header className={styles.header} id='header'>
@@ -64,7 +63,7 @@ const Header = () => {
           <img src='/icons/logo.svg' alt='logo prom visio' className={styles.logo} />
         </LinkComponent>
 
-        {isDesktop && <Links />}
+        {isDesktop && <Links location={location} />}
 
         {isDesktop && <FooterPhone borderYellow className={styles.phone} />}
 
