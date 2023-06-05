b5ad184f03fc6dd45c0aa58702831087b5fd5de0
Bombazok <sergeipiskarev@yandex.ru>
2023-06-05 17:17:47 +0300

    Add Help arrow

--- src/components/pages/home/Help.tsx
+++ src/components/pages/home/Help.tsx
@@ -1,21 +1,34 @@
 import clsx from 'clsx'
 import styles from './Help.module.scss'
-import {ReactNode, useCallback, useState} from 'react'
+import {ReactNode, useCallback, useRef, useState} from 'react'
 
 export interface HelpItem {
   title: ReactNode
   text: string
 }
 
-const HelpItem = ({title, text}: HelpItem) => {
-  const [isToggled, setIsToggled] = useState(false)
+interface HelpItemProps extends HelpItem {
+  isToggled: boolean
+  updatePointerPosition: (top: number) => void
+  onClick: () => void
+}
+
+const HelpItem = ({title, text, isToggled, updatePointerPosition, onClick}: HelpItemProps) => {
+  const itemRef = useRef<HTMLLIElement>(null)
 
   const handleClick = useCallback(() => {
-    setIsToggled((prev) => !prev)
+    onClick()
+
+    const item = itemRef.current
+    const parent = item?.parentElement
+
+    if (item && parent) {
+      updatePointerPosition(item?.getBoundingClientRect().top - parent?.getBoundingClientRect().top + 40)
+    }
   }, [])
 
   return (
-    <li className={styles.item} onClick={handleClick}>
+    <li className={styles.item} onClick={handleClick} ref={itemRef}>
       <h2>
         {title}
 
@@ -26,19 +39,41 @@ const HelpItem = ({title, text}: HelpItem) => {
         />
       </h2>
 
-      <h3 className={clsx({[styles.toggled]: isToggled})}>{text}</h3>
+      <h3
+        className={clsx({[styles.toggled]: isToggled})}
+        ref={(node) => {
+          if (node) {
+            node.style.height = node.scrollHeight + 'px'
+          }
+        }}
+      >
+        {text}
+      </h3>
     </li>
   )
 }
 
 const Help = ({items}: {items: HelpItem[]}) => {
+  const [pointerTop, setPointerTop] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState<number | null>(null)
+
+  const updatePointerPosition = useCallback((top: number) => setPointerTop(top), [])
+
   return (
     <section className={styles.help}>
       <h1>помогаем</h1>
 
       <ul className={styles.items}>
+        <div className={styles.pointer} style={{top: pointerTop}}></div>
+
         {items.map((item, index) => (
-          <HelpItem key={index} {...item} />
+          <HelpItem
+            key={index}
+            {...item}
+            updatePointerPosition={updatePointerPosition}
+            isToggled={currentIndex === index}
+            onClick={() => setCurrentIndex(index)}
+          />
         ))}
       </ul>
     </section>
