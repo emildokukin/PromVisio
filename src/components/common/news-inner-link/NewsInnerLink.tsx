89aee761ac052c547f3a1446a67da17c66b39cf0
SERGEY <sergeipiskarev@yandex.ru>
2023-06-09 18:38:58 +0300

    Use NewsPage API [WIP]

--- src/components/common/news-inner-link/NewsInnerLink.tsx
+++ src/components/common/news-inner-link/NewsInnerLink.tsx
@@ -4,15 +4,16 @@ import LinkComponent from '../link-component/LinkComponent'
 import styles from './NewsInnerLink.module.scss'
 
 interface NewsInnerLink {
-  link: string
+  link?: string
+  text?: string
   className?: string
 }
 
-const NewsInnerLink = ({link, className}: NewsInnerLink) => (
+const NewsInnerLink = ({link, text, className}: NewsInnerLink) => (
   <object className={clsx(className)}>
     <GlobeSVG className={styles.globe} />
     <LinkComponent className={styles.link} link={link} linkExternal={true}>
-      Новость на сайте Газпрома
+      {text}
     </LinkComponent>
   </object>
 )
