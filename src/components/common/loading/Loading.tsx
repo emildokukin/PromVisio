a1883f554f998e0aa76e74fdcfddc8a0f3fa4cce
Bombazok <sergeipiskarev@yandex.ru>
2023-06-09 07:12:45 +0300

    Add Loading gif

---
+++ src/components/common/loading/Loading.tsx
@@ -0,0 +1,9 @@
+import styles from './Loading.module.scss'
+
+const Loading = () => (
+  <div className={styles.loading}>
+    <img src='/icons/loading.gif' alt='loading' />
+  </div>
+)
+
+export default Loading
